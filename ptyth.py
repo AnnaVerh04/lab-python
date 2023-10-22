3405b31ec2e36be9f7f95d065f74975b3c3ad399
AnyaVerkhovaia <anyaverkhovaia@gmail.com>
2023-10-22 19:43:40 +0400

    get image

--- ptyth.py
+++ ptyth.py
@@ -1,6 +1,16 @@
 import requests
 import os
 from bs4 import BeautifulSoup
-import csv
 
-URL = "https://yandex.ru/"
+
+URL = "https://yandex.ru/images/"
+HEADERS={"User-Agent": "Mozilla/5.0"}
+
+def get_image(url, filename, index):
+ if not os.path.isdir(filename):
+  os.mkdir(filename)
+ response = requests.get(f"https:{url}", HEADERS)
+ saver = open(os.path.join(f"{filename}/{str(index).zfill(4)}.jpg"), "wb")
+ saver.write(response.content)
+ saver.close()
+
