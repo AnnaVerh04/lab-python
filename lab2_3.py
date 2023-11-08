260d99ab7ce79e522bbf2a7ea9bd16811886865e
AnyaVerkhovaia <anyaverkhovaia@gmail.com>
2023-11-08 05:24:00 +0400

    Copying a dataset

--- lab2_3.py
+++ lab2_3.py
@@ -2,42 +2,49 @@ import csv
 import os
 import shutil
 import random
-
-start_path = "dataset"
-end_path = "my_dataset2"
-file_name = "annotation.csv"
-
-def write_file(file_name, data):
-    with open(file_name, "w") as f:
-        writer = csv.writer(f, delimiter="/")
-        for row in data:
-            writer.writerow(row)
-
-data = [["full_path", "path", "class"]]
-
-random_names = []
-
-dirs = os.listdir(start_path)
-for dir in dirs:
-    path = os.path.join(start_path, dir)
-    files = os.listdir(path)
-    if not os.path.isdir(os.path.join(end_path, "dataset")):
-        os.mkdir(os.path.join(end_path, "dataset"))
-
-    for file in files:
-
-        random_file_name = random.randint(0, 10000)
-        while random_file_name in random_names:
-            random_file_name = random.randint(0, 10000)
-
-        random_names.append(random_file_name)
-
-        shutil.copyfile(os.path.join(start_path,dir,file),
-                        os.path.join(end_path, "dataset", str(random_file_name)+".jpg"))
-
-        data.append([os.path.abspath(os.path.join(end_path, "dataset", dir+"_"+file)),
-                     os.path.join(end_path, "dataset", str(random_file_name)+".jpg"), dir])
-
-write_file(os.path.join(end_path,file_name), data)
+import typing
 
 
+def write_file(file_name: str, data: list[list[str]]) -> None:
+    '''Записывает данные в csv файл'''
+    file: typing.TextIO = open(file_name, "w")
+    writer: csv.writer = csv.writer(file, delimiter=",")
+    for row in data:
+        writer.writerow(row)
+    file.close()
+
+
+def copy_and_create_annotation(start_path: str, end_path: str, file_name: str) -> None:
+    '''Копирует и создаёт файл аннотации'''
+    random_names: list[int] = []
+
+    data: list[list[str]] = [["full_path", "path", "class"]]
+    dirs: list[str] = os.listdir(start_path)
+    for dir in dirs:
+        path: str = os.path.join(start_path, dir)
+        files: list[str] = os.listdir(path)
+        if not os.path.isdir(os.path.join(end_path, "dataset")):
+            os.mkdir(os.path.join(end_path, "dataset"))
+
+        for file in files:
+
+            random_file_name: int = random.randint(0, 10000)
+            while random_file_name in random_names:
+                random_file_name: int = random.randint(0, 10000)
+
+            random_names.append(random_file_name)
+
+            shutil.copyfile(os.path.join(start_path, dir, file),
+                            os.path.join(end_path, "dataset", str(random_file_name) + ".jpg"))
+
+            data.append([os.path.abspath(os.path.join(end_path, "dataset", dir + "_" + file)),
+                         os.path.join(end_path, "dataset", str(random_file_name) + ".jpg"), dir])
+
+    write_file(os.path.join(end_path, file_name), data)
+
+
+if __name__ == "__main__":
+    start_path: str = "dataset"
+    end_path: str= "my_dataset2"
+    file_name: str = "annotation.csv"
+    copy_and_create_annotation(start_path, end_path, file_name)
