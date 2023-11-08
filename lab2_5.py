6f5487a2dab6cd2291efa1de2491a5695be6be67
AnyaVerkhovaia <anyaverkhovaia@gmail.com>
2023-11-08 06:07:58 +0400

    iterator classes

--- lab2_5.py
+++ lab2_5.py
@@ -1,35 +1,44 @@
 import csv
+import typing
+from typing import Optional
 
 
-path_to_annotation = "annotation1.csv"
-
-def read_file(path):
-    files = []
-    with open(path, "r") as csvfile:
-        reader = csv.DictReader(csvfile, delimiter="/")
-        for row in reader:
-            files.append([row["full_path"], row["class"]])
+def read_file(path: str) -> list[list[str]]:
+    '''Читает файл path и возвращает матрицу имён файлов'''
+    files: list[list[str]] = []
+    csvfile: typing.TextIO = open(path, "r")
+    reader: csv.DictReader = csv.DictReader(csvfile, delimiter=",")
+    for row in reader:
+        files.append([row["full_path"], row["class"]])
     return files
 
-class A:
-    def __init__(self, files_class, path):
-        self.file_class = files_class
-        csv_f = read_file(path)
-        self.annotation = []
+
+class FileIterator:
+    '''Класс-итератор для получения полного пути к файлу'''
+    def __init__(self, files_class: str, path: str) -> None:
+        self.file_class: str = files_class
+        csv_f: list[list[str]] = read_file(path)
+        self.annotation: list[list[str]] = []
         for i in csv_f:
             if self.file_class == i[1]:
                 self.annotation.append(i)
 
-    def __next__(self):
+    def __next__(self) -> Optional[str]:
         if self.annotation:
-            return self.annotation.pop()[0]
+            item_0: str = self.annotation[0][0]
+            self.annotation: list[list[str]] = self.annotation[1:]
+            return item_0
         else:
             return None
 
-a = A("cat", path_to_annotation)
 
-while True:
-    next_val = next(a)
-    print(next_val)
-    if next_val == None:
-        break
+if __name__ == "__main__":
+    path_to_annotation: str = "annotation.csv"
+    fileIterator: FileIterator = FileIterator("cat", path_to_annotation)
+    while True:
+        next_val: Optional[str] = next(fileIterator)
+        print(next_val)
+        if next_val == None:
+          break
+
+
