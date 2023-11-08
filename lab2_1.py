a79df1b0f3733fff0c35a2afb8c8ff29b311c8ca
AnyaVerkhovaia <anyaverkhovaia@gmail.com>
2023-11-08 05:02:17 +0400

    Creating an annotation

--- lab2_1.py
+++ lab2_1.py
@@ -1,26 +1,35 @@
 import csv
 import os
+import typing
 
 
-path_to_annotation = "annotation1.csv"
-path_data = "dataset"
+def write_file(file_name: str, data: list[list[str]]) -> None:
+    '''Записывает данные в csv файл'''
+    file: typing.TextIO = open(file_name, "w")
+    writer: csv.writer = csv.writer(file, delimiter=",")
+    for row in data:
+        writer.writerow(row)
+    file.close()
 
-def write_file(file_name, data):
-    with open(file_name, "w") as f:
-        writer = csv.writer(f, delimiter="/")
-        for row in data:
-            writer.writerow(row)
 
-data = [["full_path", "path", "class"]]
+def create_annotation(path_data: str, path_to_annotation: str) -> None:
+    '''Создаёт аннотацию'''
+    data: list[list[str]] = [["full_path", "path", "class"]]
 
-dirs = os.listdir(path_data)
+    dirs: list[str] = os.listdir(path_data)
 
-for dir in dirs:
-    path = os.path.join(path_data, dir)
-    files = os.listdir(path)
-    for file in files:
-        data.append([os.path.abspath(os.path.join(path, file)),
-                     os.path.join(path, file),
-                     dir])
+    for dir in dirs:
+        path: str = os.path.join(path_data, dir)
+        files: list[str] = os.listdir(path)
+        for file in files:
+            data.append([os.path.abspath(os.path.join(path, file)),
+                         os.path.join(path, file),
+                         dir])
 
-write_file(file_name=path_to_annotation, data= data)
+    write_file(file_name=path_to_annotation, data=data)
+
+
+if __name__ == "__main__":
+    path_to_annotation: str = "annotation.csv"
+    path_data: str = "dataset"
+    create_annotation(path_data, path_to_annotation)
