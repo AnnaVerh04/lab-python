a86fba57d4c54f7e968409afd4354ffeec530ab7
AnyaVerkhovaia <anyaverkhovaia@gmail.com>
2023-11-21 00:57:23 +0400

    Recent changes

--- main.py
+++ main.py
@@ -31,13 +31,32 @@ class MainWindow(QMainWindow):
         self.ui.button_next_dog.clicked.connect(self.click_next_dog)
         self.ui.button_get_annotation.clicked.connect(self.click_get_annotation)
 
+
+    '''Функционал кнопки для пролистывания котов
+    Parameters
+    ----------
+    self: Новый объект
+    '''
     def click_next_cat(self):
         path = next(self.cat_iterator)
         self.resize_image(path)
 
+    '''Функционал кнопки для пролистывания собак
+    Parameters
+    ----------
+    self: Новый объект
+    '''
+
     def click_next_dog(self):
         path = next(self.dog_iterator)
         self.resize_image(path)
+
+    '''Функция для масштабирования картинок
+    Parameters
+    ----------
+    self: Новый объект
+    path: Путь
+    '''
 
     def resize_image(self, path):
         pixmap = QPixmap(path)
@@ -47,11 +66,23 @@ class MainWindow(QMainWindow):
             pixmap = pixmap.scaledToHeight(self.ui.image.height())
         self.ui.image.setPixmap(pixmap)
 
+    '''Функционал для получения аннотации
+    Parameters
+    ----------
+    self: Новый объект
+    '''
+
     def click_get_annotation(self):
         filter = "csv(*.csv)"
         path = QFileDialog.getOpenFileName(filter=filter)
         self.cat_iterator = Iterator_class.FileIterator("cat", path[0])
         self.dog_iterator = Iterator_class.FileIterator("dog", path[0])
+
+    '''Функционал для создания аннотации
+    Parameters
+    ----------
+    self: Новый объект
+    '''
 
     def click_button_lab_1(self):
         path = QFileDialog.getExistingDirectory(self, "Путь к dataset")
@@ -59,7 +90,11 @@ class MainWindow(QMainWindow):
         print(os.path.relpath(path))
         Creating_a_annotations.create_annotation(os.path.relpath(path), path_to_annotation)
 
-    
+    '''Функционал копирования аннотации и dataset 
+    Parameters
+    ----------
+    self: Новый объект
+    '''
     def click_button_lab_2(self):
         start_path = QFileDialog.getExistingDirectory(self, "Путь к dataset")
         end_path = QFileDialog.getExistingDirectory(self, "Новое расположение")
@@ -68,7 +103,11 @@ class MainWindow(QMainWindow):
         Copying_a_dataset.copy_file(start_path, end_path)
         Copying_a_dataset.create_annotation(os.path.relpath(start_path), os.path.relpath(end_path), file_name)
 
-
+    '''Функционал копирования аннотации и dataset с рандомными номерами
+    Parameters
+    ----------
+    self: Новый объект
+    '''
     def click_button_lab_3(self):
         start_path = QFileDialog.getExistingDirectory(self, "Путь к dataset")
         end_path = QFileDialog.getExistingDirectory(self, "Новое расположение")
@@ -76,7 +115,11 @@ class MainWindow(QMainWindow):
         file_name: str = "annotation.csv"
         Copying_with_a_random_number.copy_and_create_annotation(os.path.relpath(start_path), os.path.relpath(end_path), file_name)
 
-
+    '''Функционал создания дерева файлов
+    Parameters
+    ----------
+    self: Новый объект
+    '''
     def click_button_tree(self):
         self.path_to_tree = QFileDialog.getExistingDirectory()
         self.dirModel = QFileSystemModel(self)
